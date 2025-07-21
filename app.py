1b9709e8e6c2abfac135b081864818a996b6a81d
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-07-21 22:45:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1697,7 +1697,7 @@ class UIComponents:
                     Analyse avancée des performances - Saison 2024-2025
                     </p>
                 <div>
-            <div style='flex: 0; text-align: right; margin-left: 32px;'>
+            <div style='flex: 0; text-align: right; margin-left: 40px;'>
                 {big5_logo_html}
             </div>
         </div>
